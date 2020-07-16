22d704d3e0edc54fcb36a5cd56ad5bfb99b84e42
Eric <nongeric@gmail.com>
2020-07-16 17:44:25 +0200

    renaming package

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 from setuptools import setup
 
 setup(
-    name="tap-forecast",
+    name="tap-forecast-v2",
     version="0.1.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
-    py_modules=["tap_forecast"],
+    py_modules=["tap_forecast-v2"],
     install_requires=[
         "singer-python>=5.0.12",
         "requests",
     ],
     entry_points="""
     [console_scripts]
-    tap-forecast=tap_forecast:main
+    tap-forecast-v2=tap_forecast:main
     """,
     packages=["tap_forecast"],
     package_data = {
